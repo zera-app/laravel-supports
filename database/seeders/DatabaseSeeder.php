dea35e4de77952f54cec68b3a892fa50766adff8
Zulfikar Ditya <52193940+zulfikar-ditya@users.noreply.github.com>
2024-10-12 11:23:41 +0700

    Install packages

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Role;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -13,11 +14,29 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        Role::create([
+            'name' => 'superuser',
+            'guard_name' => 'web',
         ]);
+
+        Role::create([
+            'name' => 'admin',
+            'guard_name' => 'web',
+        ]);
+
+        User::create([
+            'name' => 'Super User',
+            'email' => 'superuser@app.com',
+            'password' => bcrypt('password'),
+        ])->assignRole('superuser');
+
+
+        User::create([
+            'name' => 'Admin',
+            'email' => 'admin@app.com',
+            'password' => bcrypt('password'),
+        ])->assignRole('admin');
+
+        \App\Models\User::factory(100)->create();
     }
 }
